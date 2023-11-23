57b1f281e435087b8f1ec5de64288bf6ec972a04
Lumin228 <6786679@gmail.com>
2023-11-23 17:06:11 +0200

    asd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,58 @@
-import throttle from "lodash.throttle";
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const inputEmail = document.querySelector('.feedback-form input[type="email"]');
+
+form.addEventListener('submit', onFormSubmit);
+textarea.addEventListener('input', onTextareaInput);
+inputEmail.addEventListener('input', onInputEmail);
+
+const LOCAL_SAVE = "feedback-form-state";
+
+const storedData = localStorage.getItem(LOCAL_SAVE);
+const convert = storedData ? JSON.parse(storedData) : null;
+
+let data = {
+  email: "",
+  message: "",
+};
 
 
-const formA = document.querySelector('.feedback-form');
-const textArea = document.querySelector('.feedback-form textarea');
+function checkAndFillFormFields() {
 
-const feedbackForm = 'feedback-form-state';
 
-function formInput(evt){
-    const message = evt.target.value
-    localStorage.setItem(feedbackForm, message)
+ 
+  if (convert) {
+    inputEmail.value = convert.email;
+    textarea.value = convert.message;
+    data = convert;
+  }
 }
-function formSubmit(evt){
-   evt.preventDefault();
-   console.log(form)
+
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    if(data.email && data.message){
+        console.log(data);
+        localStorage.clear();
+        form.reset(); 
+        data = { email: "", message: "" }; 
+    }
+    else{
+        alert('Please fill an email and message');
+    }
 }
 
-formA.addEventListener('submit', formSubmit);
-textArea.addEventListener('input', throttle(formInput, 500));
+function onTextareaInput(evt) {
+  const value = evt.target.value; 
+  data.message = value;
+  localStorage.setItem(LOCAL_SAVE, JSON.stringify(data));
+}
+
+function onInputEmail(evt) {
+  const value = evt.target.value; 
+  data.email = value;
+  localStorage.setItem(LOCAL_SAVE, JSON.stringify(data));
+}
+
+checkAndFillFormFields()
